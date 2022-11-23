0d3c1feb8d8e94ef9798a68526cb3d36562f1017
KONSTANTINOS MAVRIDIS <kmauridis@net.geoanalysis.gr>
2022-11-23 16:07:01 +0200

    Service improvement

--- Nutrition/src/app/app.component.ts
+++ Nutrition/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Root } from './ingredient-analysis.interface';
 import { IngredientsService } from './ingredients.service';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,25 +11,26 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   i: number = 0;
   listItems: string[] = [];
-  inAna!: Observable<Root>;
+  analyzedIngredient!: Observable<Root>;
+  ingredientAnalysis!: Root;
 
   constructor(private ingredientsService: IngredientsService) {}
 
   title = 'Nutrition';
 
-  addI(newI: number) {
-    this.i = newI;
-    console.log(this.i);
-  }
   // the add button and the function
   addText(value: string) {
-    this.ingredientsService.onSearchSaveText(value);
-    this.listItems = this.ingredientsService.getList();
+    this.listItems.push(value);
   }
   //the analyze button and the function
   analyzeIngredients() {
-    this.inAna = this.ingredientsService.getIngredientList();
-    this.inAna.subscribe((data: Root)=> console.log(data))
-   
+    console.log(this.listItems)
+    for (let i of this.listItems) {
+      this.analyzedIngredient = this.ingredientsService.getIngredientList(i);
+      this.analyzedIngredient.subscribe((value:Root) => this.ingredientAnalysis = value);
+      if (this.ingredientAnalysis !== undefined) {
+        console.log(this.ingredientAnalysis.ingredients);
+      }
+    }
   }
 }
--- Nutrition/src/app/ingredients.service.ts
+++ Nutrition/src/app/ingredients.service.ts
@@ -8,28 +8,19 @@ import { Ingredient } from './Ingredients.interface';
   providedIn: 'root',
 })
 export class IngredientsService {
-  listItems: string[] = [];
+
 
   constructor(private http: HttpClient) {}
 
-  onSearchSaveText(value: string) {
-    this.listItems.push(value);
-  }
-  getList() {
-    return this.listItems;
-  }
-  getIngredientList():Observable<Root> {
-    
+  
+  getIngredientList(value:string): Observable<Root> {
     let myParams = new HttpParams()
-    .set('app_id', '405d17d3')
-    .set('app_key', 'c62d9da152ddc32578901c758fe57d78')
-    .set('nutrition-type', 'cooking')
-    .set('ingr','1%20orange%20');
-    return this.http.get<Root>(
-      'https://api.edamam.com/api/nutrition-data?app_id=405d17d3&app_key=c62d9da152ddc32578901c758fe57d78&nutrition-type=cooking&ingr=1%20orange%20'
-       //{params: myParams}
-    ).pipe(filter(data => for(let k in data){
-      
-    }))
+      .set('app_id', '405d17d3')
+      .set('app_key', 'c62d9da152ddc32578901c758fe57d78')
+      .set('nutrition-type', 'cooking')
+      .set('ingr', value);
+    return this.http.get<Root>('https://api.edamam.com/api/nutrition-data', {
+      params: myParams,
+    });
   }
 }
